3fe83c54ecc4e6a4a0060557515b225ef19c7dd7
wajdi112233 <dicewajdi@gmail.com>
2025-07-31 15:28:14 +0100

    application changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import items from "@/components/hanout/card.json";
-import Cards from "@/components/Cards";
 import Hanout from "@/components/hanout";
-import type { CardItem } from "@/utile/types/interfaces";
 
 export default function Home() {
   return (
     <div className="bg-gray-900 min-h-screen">
       <Hanout />
-      <Cards data={items as CardItem[]} cols={4} className="p-8" />
     </div>
   );
 }
